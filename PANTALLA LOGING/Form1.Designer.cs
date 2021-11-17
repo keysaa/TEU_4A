4201a44f8dca43ed4d026f2508b83ed4fe0e54b9
unknown <salasalejandro368@gmail.com>
2021-11-17 15:13:49 -0600

    pantallalogin

---
+++ PANTALLA LOGING/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace PANTALLA_LOGING
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.USUARIO = new System.Windows.Forms.TextBox();
+            this.CONTRASEÑA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(412, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Aceptar ";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // USUARIO
+            // 
+            this.USUARIO.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.USUARIO.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.USUARIO.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.USUARIO.Location = new System.Drawing.Point(297, 61);
+            this.USUARIO.Name = "USUARIO";
+            this.USUARIO.Size = new System.Drawing.Size(190, 26);
+            this.USUARIO.TabIndex = 2;
+            this.USUARIO.Text = "Usuario";
+            // 
+            // CONTRASEÑA
+            // 
+            this.CONTRASEÑA.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.CONTRASEÑA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CONTRASEÑA.Location = new System.Drawing.Point(297, 110);
+            this.CONTRASEÑA.Name = "CONTRASEÑA";
+            this.CONTRASEÑA.Size = new System.Drawing.Size(190, 26);
+            this.CONTRASEÑA.TabIndex = 3;
+            this.CONTRASEÑA.Text = "Contraseña ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(359, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "LOGIN";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PANTALLA_LOGING.Properties.Resources.kisspng_padlock_animation_image_idea_5be64fcdaa6073_1184660215418203656979;
+            this.pictureBox1.Location = new System.Drawing.Point(78, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 158);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(560, 229);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CONTRASEÑA);
+            this.Controls.Add(this.USUARIO);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox USUARIO;
+        private System.Windows.Forms.TextBox CONTRASEÑA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
